7022ff7f67954b3cbdb8025998efb9960d593cc0
Eugene Sorokin <jeksor.cover@gmail.com>
2018-07-21 20:55:36 +0100

    Added ugly chart
    Signed-off-by: Eugene Sorokin <jeksor.cover@gmail.com>

--- easyinvest-mobile/app/src/main/java/com/easyinvest/ui/TraderDetailsActivity.kt
+++ easyinvest-mobile/app/src/main/java/com/easyinvest/ui/TraderDetailsActivity.kt
@@ -1,19 +1,44 @@
 package com.easyinvest.ui
 
+import android.animation.PropertyValuesHolder
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
+import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.animation.BounceInterpolator
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.db.chart.animation.Animation
+import com.db.chart.model.LineSet
+import com.db.chart.renderer.AxisRenderer
+import com.db.chart.tooltip.Tooltip
+import com.db.chart.util.Tools
 import com.easyinvest.R
 import com.easyinvest.data.Trader
-import kotlinx.android.synthetic.main.activity_trader_details.*
 import kotlinx.android.synthetic.main.trader_header.*
+import kotlin.math.absoluteValue
+
 
 private const val TRADER_KEY = "TRADER_KEY"
+
+fun TextView.setTextViewDrawableColor(color: Int) {
+    for (drawable in compoundDrawables) {
+        if (drawable != null) {
+            drawable.colorFilter =
+                    PorterDuffColorFilter(
+                        ContextCompat.getColor(context, color),
+                        PorterDuff.Mode.SRC_IN
+                    )
+        }
+    }
+}
 
 class TraderDetailsActivity : AppCompatActivity() {
 
@@ -28,36 +53,93 @@ class TraderDetailsActivity : AppCompatActivity() {
 
     private val trader by lazy { fromIntent(intent) }
 
+    private val labels = arrayOf("Jan", "Fev", "Mar", "Apr", "Jun", "May", "Jul", "Aug", "Sep")
+
+    private val values = listOf(5, 6, 4, 8, 6, 9, 11, 13, 7).map { it.toFloat() }.toFloatArray()
+
+    private lateinit var ttooltip: Tooltip
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trader_details)
-        setSupportActionBar(toolbar)
 
-        toolbar_layout.title = trader.name
+        name.text = trader.name
 
         Glide.with(this)
             .load(trader.avatar)
-            .apply(RequestOptions.circleCropTransform()).into(avatar)
+            .apply(RequestOptions.circleCropTransform())
+            .into(avatar)
+
+        val color = if (trader.profitPercentage >= 0) {
+            R.color.green
+        } else R.color.red
+
+        with(profit) {
+            setTextColor(ContextCompat.getColor(this@TraderDetailsActivity, color))
+            setTextViewDrawableColor(color)
+            profit.text = trader.profitPercentage.absoluteValue.toString()
+        }
 
         updateSubscribeIcon(trader.followedByCurrentInvestor)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(
-                view,
-                "You have successfully ${if (trader.followedByCurrentInvestor) "subscribed" else "unsubscribed"}!",
-                Snackbar.LENGTH_LONG
-            ).show()
+        // Tooltip
+        ttooltip = Tooltip(this, R.layout.linechart_three_tooltip, R.id.value)
 
-            updateSubscribeIcon(!trader.followedByCurrentInvestor)
+        ttooltip.setVerticalAlignment(Tooltip.Alignment.BOTTOM_TOP)
+        ttooltip.setDimensions(Tools.fromDpToPx(58f).toInt(), Tools.fromDpToPx(25f).toInt())
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+
+            ttooltip.setEnterAnimation(
+                PropertyValuesHolder.ofFloat(View.ALPHA, 1f),
+                PropertyValuesHolder.ofFloat(View.SCALE_Y, 1f),
+                PropertyValuesHolder.ofFloat(View.SCALE_X, 1f)
+            ).setDuration(200)
+
+            ttooltip.setExitAnimation(
+                PropertyValuesHolder.ofFloat(View.ALPHA, 0f),
+                PropertyValuesHolder.ofFloat(View.SCALE_Y, 0f),
+                PropertyValuesHolder.ofFloat(View.SCALE_X, 0f)
+            ).setDuration(200)
+
+            ttooltip.setPivotX(Tools.fromDpToPx(65f) / 2)
+            ttooltip.setPivotY(Tools.fromDpToPx(25f))
         }
+
+        // Data
+        var dataset = LineSet(labels, values)
+        dataset.setColor(Color.parseColor("#758cbb"))
+            .setFill(Color.parseColor("#9C152949"))
+            .setDotsColor(Color.parseColor("#758cbb"))
+            .setThickness(4f)
+            .setDashed(floatArrayOf(10f, 10f))
+            .beginAt(5)
+        chart.addData(dataset)
+
+        dataset = LineSet(labels, values)
+        dataset.setColor(Color.parseColor("#b3b5bb"))
+            .setFill(Color.parseColor("#9C152949"))
+            .setDotsColor(Color.parseColor("#ffc755"))
+            .setThickness(4f)
+            .endAt(6)
+        chart.addData(dataset)
+
+        val chartAction = Runnable {
+            ttooltip.prepare(chart.getEntriesArea(0).get(3), values[3])
+            chart.showTooltip(ttooltip, true)
+        }
+
+        chart.setXLabels(AxisRenderer.LabelPosition.NONE)
+            .setYLabels(AxisRenderer.LabelPosition.NONE)
+            .setTooltips(ttooltip)
+            .show(
+                Animation().setInterpolator(BounceInterpolator())
+                    .fromAlpha(0)
+                    .withEndAction(chartAction)
+            )
     }
 
     private fun updateSubscribeIcon(followedByCurrentInvestor: Boolean) {
-        fab.setImageDrawable(
-            ContextCompat.getDrawable(
-                this,
-                if (followedByCurrentInvestor) R.drawable.ic_unsubscribe else R.drawable.ic_subscribe
-            )
-        )
+
     }
 }
