3b6aa81f799417d512a097ab0acbeabc5768664b
Kirill Makarov <kir-maka@yandex.ru>
2018-07-21 17:13:14 +0100

    set title to tabs

--- easyinvest-mobile/app/src/main/java/com/easyinvest/MainTabbedActivity.kt
+++ easyinvest-mobile/app/src/main/java/com/easyinvest/MainTabbedActivity.kt
@@ -12,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_main_tabbed.*
 class MainTabbedActivity : AppCompatActivity() {
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        title = item.title
         when (item.itemId) {
             R.id.navigation_home -> {
                 loadFragment(PortfolioFragment.instance())
@@ -26,14 +27,18 @@ class MainTabbedActivity : AppCompatActivity() {
                 return@OnNavigationItemSelectedListener true
             }
         }
+
         false
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_tabbed)
         navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+
         loadFragment(PortfolioFragment.instance())
+        setTitle(R.string.home_tab_title)
     }
 
     private fun loadFragment(fragment: Fragment) {
