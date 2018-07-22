67f08b71848d3e52909d70b72cd30dd59c2f7189
Kirill Makarov <kir-maka@yandex.ru>
2018-07-22 09:39:13 +0100

    make popular trader clickable

--- easyinvest-mobile/app/src/main/java/com/easyinvest/traders/PopularTraderItem.kt
+++ easyinvest-mobile/app/src/main/java/com/easyinvest/traders/PopularTraderItem.kt
@@ -1,6 +1,19 @@
 package com.easyinvest.traders
 
 import com.easyinvest.base.DisplayableItem
+import com.easyinvest.data.Trader
 import com.easyinvest.portfolio.items.TraderItem
 
-data class PopularTraderItem(val item: TraderItem, val monthlyPercent: Int) : DisplayableItem
+data class PopularTraderItem(val item: TraderItem, val monthlyPercent: Int) : DisplayableItem {
+    fun toTrader(): Trader {
+        return Trader(
+                id = item.id,
+                name = item.name,
+                avatar = item.avatar,
+                profitPercentage = monthlyPercent,
+                pricePerMonth = 10f,
+                followersCount = 334,
+                followedByCurrentInvestor = true
+        )
+    }
+}
